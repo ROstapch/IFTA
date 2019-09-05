f1f9ac68f9259d7d9fb3028e5fac935aa83cda07
ROstapch <ostapchukroma@gmail.com>
2019-09-06 01:24:47 +0300

    Daily_Location and Fuel models changed from datetime to date fields

--- IFTA/Reporting/models.py
+++ IFTA/Reporting/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Daily_Location(models.Model):
 	truck = models.ForeignKey('Core.Unit', on_delete = models.PROTECT, help_text='Link to the related unit(vehicle) data')
 
-	date = models.DateTimeField(help_text='Date (mm/dd/yyyy)')
+	date = models.DateField(help_text='Date (mm/dd/yyyy)') #convert to yyyy-mm-dd when saving
 	jurisdiction = models.CharField(max_length=10, choices=Jurisdiction_list.jurisdiction_choice, help_text='Jurisdiction abbreviation')
 	miles = models.PositiveIntegerField(help_text='Miles at current jurisdiction')
 	timezone = models.CharField(max_length=30, default = TimeZones_List.utc_5, choices=TimeZones_List.tz_choice, help_text='Timezone used when getting data')
@@ -29,7 +29,7 @@ class Fuel(models.Model):
 	truck = models.ForeignKey('Core.Unit', on_delete = models.PROTECT, help_text='Link to the related unit(vehicle) data')
 	driver = models.ForeignKey('Core.Driver', default=None,null=True, blank=True, on_delete = models.SET_DEFAULT, help_text='Link to related the driver data')
 
-	date = models.DateTimeField(help_text='Date (mm/dd/yyyy)')
+	date = models.DateField(help_text='Date (mm/dd/yyyy)') #convert to yyyy-mm-dd when saving
 	jurisdiction = models.CharField(max_length=10, choices=Jurisdiction_list.jurisdiction_choice, help_text='Jurisdiction abbreviation')
 
 	fuel_amount = models.FloatField(default=0.0, help_text='Volume of fuel bought')
@@ -37,7 +37,7 @@ class Fuel(models.Model):
 
 	ref_no = models.CharField(max_length=15, null=True, blank=True, help_text='Invoice number')
 	vendor = models.CharField(max_length=40, help_text='Truck stop name')
-	
+
 
 
 
@@ -50,8 +50,8 @@ class Jurisdiction(models.Model):
 
 
 class Total_Report(models.Model):
-	start_date = models.DateField(help_text='Start date of the report')
-	end_date = models.DateField(help_text='Ending date of the report')
+	start_date = models.DateField(help_text='Start date of the report (mm/dd/yyyy)') #convert to yyyy-mm-dd when saving
+	end_date = models.DateField(help_text='Ending date of the report (mm/dd/yyyy)') #convert to yyyy-mm-dd when saving
 	done_by = models.ForeignKey(User, default=None, null = True, blank=True, on_delete=models.SET_DEFAULT)
 
 	truck = models.ForeignKey('Core.Unit', default=None, on_delete = models.PROTECT, help_text='Link to the related unit(vehicle) data')
